a63c6210c2c40c1dfb459094c80564398e6d3398
Joel Mendivil <jmendivil171@west-mec.org>
2021-04-19 15:21:01 -0700

    Need to add AND remove and event listener for the project to be done

--- voteIndex.js
+++ voteIndex.js
@@ -1,29 +1,43 @@
-function nixonMouseOver(){
+//When page loads up, it automatically displays the following paragraph in the HTML
+window.onload = () => {
+    var para = document.createElement("p");
+    var node = document.createTextNode("A new paragraph using JavaScript!");
+    para.appendChild(node);
+
+    var element = document.getElementById("div1");
+    element.appendChild(para);
+}
+
+//Calculates the position of the mouse pointer when hovering over the vote buttons
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coor;
+}
+
+function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+}
+
+//Changes the color of the paragraph when clicked on by the user
+function thisFunction(elmnt, clr) {
+    elmnt.style.color = clr;
+}
+
+//Changes the images of the vote buttons when being hovered on by the user
+function nixonMouseOver() {
     document.getElementById('betterNixon').src = "Images/betternixon.png";
 }
 
-function nixonMouseLeave(){
+function nixonMouseLeave() {
     document.getElementById('betterNixon').src = "Images/nixon.jpeg";
 }
 
-function mcGovernMouseOver(){
-    document.getElementById('betterMcGovern').src = "Images/";
+function mcGovernMouseOver() {
+    document.getElementById('betterMcGovern').src = "Images/bettermcgovern.png";
 }
 
-function mcGovernMouseLeave(){
-    document.getElementById('betterMcGovern').src = "Images/";
+function mcGovernMouseLeave() {
+    document.getElementById('betterMcGovern').src = "Images/mcgovern.png";
 }
-
-document.body.onload = addElement;
-
-function addElement () {
-  const newDiv = document.createElement("div");
-  const newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent);
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
-
-window.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-  }) 
