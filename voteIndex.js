302f9dc69f92c32e9f3d46493f1a7eddf924e26e
Joel Mendivil <jmendivil171@west-mec.org>
2021-04-20 15:25:27 -0700

    Done. I may add final touches depending.

--- voteIndex.js
+++ voteIndex.js
@@ -1,11 +1,12 @@
 //When page loads up, it automatically displays the following paragraph in the HTML
 window.onload = () => {
     var para = document.createElement("p");
-    var node = document.createTextNode("A new paragraph using JavaScript!");
+    var node = document.createTextNode("A new paragraph was created using JavaScript! Also, try to right click the title.");
     para.appendChild(node);
 
     var element = document.getElementById("div1");
     element.appendChild(para);
+    alert("Things may not look as what it may seem! You've been warned.")
 }
 
 //Calculates the position of the mouse pointer when hovering over the vote buttons
@@ -24,6 +25,32 @@ function clearCoor() {
 function thisFunction(elmnt, clr) {
     elmnt.style.color = clr;
 }
+
+//Results after selecting one president
+document.getElementById('nixon').addEventListener("click", nixonVote);
+
+function nixonVote(){
+    alert("You have voted for McGovern. Thank you :)");
+    alert("SIKE!!!! REJECT PRESIDENTS!! EMBRACE CRAIG");
+    document.getElementById('nixon').removeEventListener("click", nixonVote);
+    document.body.style.backgroundImage = "url('Images/craig.png')";
+}
+
+document.getElementById('mcgovern').addEventListener("click", mcGovernVote)
+
+function mcGovernVote(){
+    alert("You have voted for Nixon. Thank you :)");
+    alert("SIKE!!!! REJECT PRESIDENTS!! EMBRACE CRAIG");
+    document.getElementById('mcgovern').removeEventListener("click", mcGovernVote);
+    document.body.style.backgroundImage = "url('Images/craig.png')";
+}
+
+//Prevents the title of the page from being right clicked
+const noContext = document.getElementById('noContextMenu');
+
+noContext.addEventListener('contextmenu', e => {
+  e.preventDefault();
+});
 
 //Changes the images of the vote buttons when being hovered on by the user
 function nixonMouseOver() {
